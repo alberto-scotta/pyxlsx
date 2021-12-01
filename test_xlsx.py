21a17372c49e83857a610125cce37fe4fd4fc0d4
Alberto Scotta <alberto.scotta@saetsrl.com>
2021-12-01 11:42:38 +0100

    Add function to write content of cells

--- test_xlsx.py
+++ test_xlsx.py
@@ -1,28 +1,37 @@
 import xlsx
+import pandas
+import shutil
 
-DATABASE = "pwddatabase.xlsx"
+DATABASE_BACKUP = "pwddatabase.xlsx"
+DATABASE = "pwddatabase.tmp.xlsx"
+# Copy the file to something else that I do not keep track of and I feel
+# free to edit
+shutil.copy(DATABASE_BACKUP, DATABASE)
 PWD_COL = "B"
 xlsx = xlsx.Xlsx(DATABASE)
 
+# I change the content of the file only temporarly
+xlsx.write_cell("A6", "riccardo")
+
 while True:
-    name = raw_input("Give me the name whose password you wanna know: ")
+    name = input("Give me the name whose password you wanna know: ")
     cell = xlsx.get_cell(name)
     if None != cell:
         break
     else:
-        print "Couldn't find name, are you sure it is correct? try again."
+        print("Couldn't find name, are you sure it is correct? try again.")
 
 row = ""
 for char in cell:
 	if True == char.isdigit():
 		row += char
 
-print xlsx.get_content(PWD_COL + row)
+print(xlsx.get_content(PWD_COL + row))
 
 cell = "D12"
 
-print ""
-print "Content of " + cell + ":"
-print xlsx.get_content(cell)
+print("")
+print("Content of " + cell + ":")
+print(xlsx.get_content(cell))
 
-print "Bye!"
+print("Bye!")
--- xlsx/xlsx.py
+++ xlsx/xlsx.py
@@ -1,15 +1,21 @@
 import zipfile
 import xml.dom.minidom as minidom
+import tempfile
+import os
+
+shared_strings_path = "xl/sharedStrings.xml"
+sheet1_path = "xl/worksheets/sheet1.xml"
 
 class Xlsx:
     def __init__(self, filename):
-        zip_file = zipfile.ZipFile(filename)
+        self.filename = filename
+        self.zip_file = zipfile.ZipFile(filename)
 
-        shared_strings = zip_file.open("xl/sharedStrings.xml")
-        sheet = zip_file.open("xl/worksheets/sheet1.xml")
+        self.shared_strings_file = self.zip_file.open(shared_strings_path)
+        self.sheet_file = self.zip_file.open(sheet1_path)
 
-        self.shared_strings = minidom.parse(shared_strings)
-        self.sheet = minidom.parse(sheet)
+        self.shared_strings = minidom.parse(self.shared_strings_file)
+        self.sheet = minidom.parse(self.sheet_file)
 
     # return string if text cell, None otherwise
     def get_content(self, cell):
@@ -58,3 +64,50 @@ class Xlsx:
         t_list = self.shared_strings.getElementsByTagName("si")
         return t_list[index].firstChild.firstChild.data
 
+    def __del__(self):
+        self.shared_strings_file.close()
+        self.sheet_file.close()
+        self.zip_file.close()
+
+    # can only write string cells
+    def write_cell(self, cell, string):
+        # Extract xlsx in a temp folder
+        with tempfile.TemporaryDirectory() as tmpdirname:
+            # Created temporary directory tmpdirname
+            pass
+        zip_file = zipfile.ZipFile(self.filename)
+        zip_file.extractall(tmpdirname)
+        zip_file.close()
+
+        # Change the string file
+        cell = cell.strip()
+        c_list = self.sheet.getElementsByTagName("c")
+        for c in c_list:
+            if c.getAttribute("r") == cell:
+                try:
+                    content = c.firstChild.firstChild.data
+                except AttributeError:
+                    # void cell
+                    return False
+                if c.hasAttribute("t") and c.getAttribute("t") == "s":
+                    self.__write_string_from_index(int(content), string)
+
+        # Open string file in write
+        shared_strings_long_path = os.path.join(tmpdirname, shared_strings_path)
+        with open(shared_strings_long_path, "w") as shared_strings_file:
+            shared_strings_file.write(self.shared_strings.toxml())
+
+        # Zip content of the folder overwritting old file
+        zip_file = zipfile.ZipFile(self.filename, "w")
+
+        for root, dirs, files in os.walk(tmpdirname):
+            for file in files:
+                # remove path up to tmpdirname
+                complete_path = os.path.join(root, file)
+                rel_path = os.path.relpath(complete_path, tmpdirname)
+                zip_file.write(complete_path, arcname=rel_path)
+
+
+    def __write_string_from_index(self, index, string):
+        t_list = self.shared_strings.getElementsByTagName("si")
+        t_list[index].firstChild.firstChild.data = string
